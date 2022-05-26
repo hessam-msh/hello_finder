af7290910939abff66406e80f8b2b4cc208d0237
hessam-msh <hessam.m.sharifi@gmail.com>
2022-05-26 10:16:34 +0430

    adding a second code as a comment

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ text = input().lower()  # to become case-insensitive
 to_find = "hello"  # u can change this word to whatever u want!
 
 print(check_string(text, to_find))
+
+"""
+Second code 
+"""
+
+# text = input().lower()
+# h = text.find('h')
+# res = False
+# if h != -1:
+#     e = text.find('e', h + 1)
+#     if e != -1:
+#         l1 = text.find('l', e + 1)
+#         if l1 != -1:
+#             l2 = text.find('l', l1 + 1)
+#             if l2 != -1:
+#                 o = text.find('o', l2 + 1)
+#                 if o != -1:
+#                     res = True
+# print('YES' if res is True else 'NO')
